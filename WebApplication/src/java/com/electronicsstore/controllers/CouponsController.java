d7cd6d8c1e5181504766014cf5ead508917be33a
ejunior01 <jk.jr47@gmail.com>
2024-11-16 14:54:11 -0300

    Adicionado a validação para quando o expirationAt é null

--- WebApplication/src/java/com/electronicsstore/controllers/CouponsController.java
+++ WebApplication/src/java/com/electronicsstore/controllers/CouponsController.java
@@ -77,7 +77,7 @@ public class CouponsController extends HttpServlet {
             double discountValue = Double.parseDouble(request.getParameter("discountValue"));
             Date startAt = sdf.parse(request.getParameter("startAt"));
 
-            if (!request.getParameter("expirationAt").isBlank()) {
+            if (request.getParameter("expirationAt") != null && !request.getParameter("expirationAt").isBlank()) {
                 expirationAt = sdf.parse(request.getParameter("expirationAt"));
             }
 
@@ -162,7 +162,7 @@ public class CouponsController extends HttpServlet {
         String description = request.getParameter("description");
         double discountValue = Double.parseDouble(request.getParameter("discountValue"));
         Date startAt = sdf.parse(request.getParameter("startAt"));
-        
+
         if (request.getParameter("expirationAt") != null && !request.getParameter("expirationAt").isBlank()) {
             expirationAt = sdf.parse(request.getParameter("expirationAt"));
         }
